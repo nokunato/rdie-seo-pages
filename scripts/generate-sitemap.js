f346c9581e3e82af5c01dd999174c271829db2b2
nokunato <55089026+nokunato@users.noreply.github.com>
2025-07-09 14:55:37 +0100

    Update generate-sitemap.js

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 
-const baseUrl = 'https://removeduplicatesinexcel.com/seo';
+const baseUrl = 'https://seo.removeduplicatesinexcel.com/seo';
 const seoFolder = './seo';
 const outputFile = './sitemap.xml';
 
